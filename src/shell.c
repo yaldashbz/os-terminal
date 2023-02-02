0ca8d463be263e6deb12b3d5cff4fd1383a4739e
yasamanzolfi <yasamanzolfi4@gmail.com>
2023-02-02 20:46:17 +0330

    call split_into_cammands in shell.c file

--- src/shell.c
+++ src/shell.c
@@ -16,56 +16,57 @@
 char *BATCH = "batch";
 char *INTERACTIVE = "interactive";
 
-int shell_help(tok_t arg[]);
-int shell_cd(tok_t arg[]);
-int shell_pwd(tok_t arg[]);
-int shell_wait(tok_t arg[]);
-int shell_quit(tok_t arg[]);
+int shell_help(char *arg[]);
+int shell_cd(char *arg[]);
+int shell_pwd(char *arg[]);
+int shell_wait(char *arg[]);
+int shell_quit(char *arg[]);
 
-typedef int command_fun (tok_t args[]);
+typedef int command_fun (char *arg[]);
 typedef struct fun_desc {
-  command_fun *fun;
-  char *cmd;
-  char *doc;
-  int param_num;
+    command_fun *fun;
+    char *cmd;
+    char *doc;
+    int param_num;
 } fun_desc_t;
 
 fun_desc_t cmd_table[] = {
-  {help_command, "?", "show the help menu"},
-  {quit_command, "quit", "quit the command shell"},
-  {cd_command, "cd", "go to a directory"},
-  {pwd_command, "pwd", "get the working directory address"}
+    {help_command, "?", "show the help menu"},
+    {quit_command, "quit", "quit the command shell"},
+    {cd_command, "cd", "go to a directory"},
+    {pwd_command, "pwd", "get the working directory address"}
 };
 
-int quit_command(tok_t arg[]) {
-  printf("Quitting\n");
-  exit(0);
-  return 1;
+int quit_command(char *arg[]){
+    printf("Quitting\n");
+    exit(0);
+    return 1;
 }
 
-int help_command(tok_t arg[]) {
-  for (int i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
-    printf("%s: %s\n",cmd_table[i].cmd, cmd_table[i].doc);
-  }
-  return 1;
+int help_command(char *arg[]) {
+    for (int i=0; i < (sizeof(cmd_table)/sizeof(fun_desc_t)); i++) {
+        printf("%s: %s\n",cmd_table[i].cmd, cmd_table[i].doc);
+    }
+    return 1;
 }
 
-int cd_command(tok_t arg[]){
-  char* new_dir = arg[0];
-  chdir(new_dir);
-  return 1;
+int cd_command(char *arg[]){
+    char* new_dir = arg[0];
+    chdir(new_dir);
+    return 1;
 }
 
-int pwd_command(tok_t arg[]){
-  char current_path[500];
-  printf(getcwd(current_path, sizeof(current_path)));
-  return 1;
+int pwd_command(char *arg[]){
+    char current_path[500];
+    printf(getcwd(current_path, sizeof(current_path)));
+    return 1;
 }
 
 int start_shell(FILE *input_file){
     char *input_line;
     while(1){
-        input_line = read_line(input_file)
+        input_line = read_line(input_file);
+        Token_desc commands_t = split_into_commands(input_line);
     }
     return 1;
 }
